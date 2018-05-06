86fea92ad2b74ef7aceb1c8d8fcac0a40809eba0
James Knight <knightjdr@gmail.com>
2018-05-06 15:59:40 -0400

    Fix uglifiy option in demo webpack

--- config/webpack.config.demo.js
+++ config/webpack.config.demo.js
@@ -239,7 +239,14 @@ module.exports = {
     new webpack.DefinePlugin(env.stringified),
     // Minify the code.
     new webpack.optimize.UglifyJsPlugin({
-      compress: false,
+      compress: {
+        warnings: false,
+        // Disabled because of an issue with Uglify breaking seemingly valid code:
+        // https://github.com/facebookincubator/create-react-app/issues/2376
+        // Pending further investigation:
+        // https://github.com/mishoo/UglifyJS2/issues/2011
+        comparisons: false,
+      },
       mangle: {
         safari10: true
       },
